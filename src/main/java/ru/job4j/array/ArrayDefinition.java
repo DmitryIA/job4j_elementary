07d6cb9c234f2266aa122a043591078b9729fc88
Dmitry <chest@MacBook-Pro-Dmitry.local>
2025-12-26 01:08:41 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,12 +2,6 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        System.out.println("Размер масcива равен: " + ages.length);
-        String[] surnames = new String[100500];
-        System.out.println("Размер масcива равен: " + surnames.length);
-        float[] prices = new float[40];
-        System.out.println("Размер масcива равен: " + prices.length);
         String[] names = new String[4];
         names [0] = "Dmitry";
         names [1] = "Sergey";
